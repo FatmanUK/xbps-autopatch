4e1f59b88dc3b3dfb7f6ec36b6e5afbd4a1634d2
Adam J. Richardson <fatman.uk@gmail.com>
2020-03-25 15:09:57 +0000

    Corrected permissions on installed xbps-static dir.

--- xbps-static.bash
+++ xbps-static.bash
@@ -3,7 +3,7 @@
 cd /opt
 rm xbps-static-static-0.56_5.x86_64-musl.tar.xz
 wget https://alpha.de.repo.voidlinux.org/static/xbps-static-static-0.56_5.x86_64-musl.tar.xz
-mkdir xbps-static-static-0.56_5
+install -d -m0755 xbps-static-static-0.56_5
 cd -
 cd /opt/xbps-static-static-0.56_5
 tar xvpJf ../xbps-static-static-0.56_5.x86_64-musl.tar.xz
